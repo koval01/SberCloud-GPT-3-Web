bc276e1d28cd8ea32263541a64476317f40e40c5
Koval <koval@q-writer.com>
2021-07-28 19:12:06 +0300

    Update core.js

--- core.js
+++ core.js
@@ -27,6 +27,20 @@ function edit_query_string_q(data) {
   history.replaceState(null, null, "?" + queryParams.toString());
 }
 
+function getParameterByName(name, url = window.location.href) {
+    try {
+        name = name.replace(/[\[\]]/g, '\\$&');
+        var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    } catch {
+        alert("Ошибка парсинга адресной строки.");
+    }
+}
+
+
 function format_result(server_response) {
   let text = (server_response.predictions).replace("\n", "<br/>");
   let html_result = `<figure class="border-bottom" onclick="select_story(this)"><blockquote class="blockquote"><b>${text}</b></blockquote><figcaption class="blockquote-footer">SberCloud</figcaption></figure>`;
